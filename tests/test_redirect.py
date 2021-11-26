8cc89117215dace4200200d57dc278bd6ed1f032
lituarte <lautaro.javier.ituarte@gmail.com>
2021-11-26 18:48:11 -0300

    Agrego tests del redirect post

--- tests/test_redirect.py
+++ tests/test_redirect.py
@@ -11,6 +11,7 @@ async def succesful_get_current_active_user():
     return {"username": "test@test.com"}
 
 
+# TESTS REDIRECT DEL GET
 @mock.patch("src.routers.redirect.httpx")
 def test_get_redirect_401_sin_jwt(mock_httpx):
     app.dependency_overrides = {}
@@ -35,11 +36,54 @@ def test_get_redirect_arma_bien_url_cursos(mock_httpx):
     response = client.get("/redirect/cursos/test?asd=1")
     assert response.status_code == 200
     assert mock_httpx.get.call_args[0][0] == os.getenv("API_CURSOS_URL") or "" + "/test?asd=1"
+    assert "Authorization" in str(mock_httpx.get.call_args[1])
+    assert "X-API-KEY" in str(mock_httpx.get.call_args[1])
 
 
 @mock.patch("src.routers.redirect.httpx")
 def test_get_redirect_400_api_inexistente(mock_httpx):
     app.dependency_overrides[get_current_active_user] = succesful_get_current_active_user
     response = client.get("/redirect/api_test/test?asd=1")
+    assert response.status_code == 400
+    assert not mock_httpx.called
+
+
+# TESTS REDIRECT DEL POST
+@mock.patch("src.routers.redirect.httpx")
+def test_post_redirect_401_sin_jwt(mock_httpx):
+    app.dependency_overrides = {}
+    response = client.post("/redirect/usuarios/test")
+    assert response.status_code == 401
+    assert not mock_httpx.called
+
+
+@mock.patch("src.routers.redirect.httpx")
+def test_post_redirect_arma_bien_url_usuarios(mock_httpx):
+    app.dependency_overrides[get_current_active_user] = succesful_get_current_active_user
+    body = {"test": "test"}
+    response = client.post("/redirect/usuarios/test", json=body)
+    assert response.status_code == 200
+    assert mock_httpx.post.call_args[0][0] == os.getenv("API_USUARIOS_URL") or "" + "/test"
+    assert "Authorization" in str(mock_httpx.post.call_args[1])
+    assert "X-API-KEY" in str(mock_httpx.post.call_args[1])
+    assert mock_httpx.post.call_args[1].get("json") == body
+
+
+@mock.patch("src.routers.redirect.httpx")
+def test_post_redirect_arma_bien_url_cursos(mock_httpx):
+    app.dependency_overrides[get_current_active_user] = succesful_get_current_active_user
+    body = {"test": "test"}
+    response = client.post("/redirect/cursos/test", json=body)
+    assert response.status_code == 200
+    assert mock_httpx.post.call_args[0][0] == os.getenv("API_CURSOS_URL") or "" + "/test"
+    assert "Authorization" in str(mock_httpx.post.call_args[1])
+    assert "X-API-KEY" in str(mock_httpx.post.call_args[1])
+    assert mock_httpx.post.call_args[1].get("json") == body
+
+
+@mock.patch("src.routers.redirect.httpx")
+def test_post_redirect_400_api_inexistente(mock_httpx):
+    app.dependency_overrides[get_current_active_user] = succesful_get_current_active_user
+    response = client.post("/redirect/api_test/test")
     assert response.status_code == 400
     assert not mock_httpx.called
