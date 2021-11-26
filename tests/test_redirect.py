e79d1b3a1b8679fb16be02a39c3f12c331f67e7d
lituarte <lautaro.javier.ituarte@gmail.com>
2021-11-26 18:54:37 -0300

    Agrego tests del redirect delete

--- tests/test_redirect.py
+++ tests/test_redirect.py
@@ -87,3 +87,40 @@ def test_post_redirect_400_api_inexistente(mock_httpx):
     response = client.post("/redirect/api_test/test")
     assert response.status_code == 400
     assert not mock_httpx.called
+
+
+# TESTS REDIRECT DEL DELETE
+@mock.patch("src.routers.redirect.httpx")
+def test_post_redirect_401_sin_jwt(mock_httpx):
+    app.dependency_overrides = {}
+    response = client.delete("/redirect/usuarios/test")
+    assert response.status_code == 401
+    assert not mock_httpx.called
+
+
+@mock.patch("src.routers.redirect.httpx")
+def test_delete_redirect_arma_bien_url_usuarios(mock_httpx):
+    app.dependency_overrides[get_current_active_user] = succesful_get_current_active_user
+    response = client.delete("/redirect/usuarios/test")
+    assert response.status_code == 200
+    assert mock_httpx.delete.call_args[0][0] == os.getenv("API_USUARIOS_URL") or "" + "/test"
+    assert "Authorization" in str(mock_httpx.delete.call_args[1])
+    assert "X-API-KEY" in str(mock_httpx.delete.call_args[1])
+
+
+@mock.patch("src.routers.redirect.httpx")
+def test_delete_redirect_arma_bien_url_cursos(mock_httpx):
+    app.dependency_overrides[get_current_active_user] = succesful_get_current_active_user
+    response = client.delete("/redirect/cursos/test")
+    assert response.status_code == 200
+    assert mock_httpx.delete.call_args[0][0] == os.getenv("API_CURSOS_URL") or "" + "/test"
+    assert "Authorization" in str(mock_httpx.delete.call_args[1])
+    assert "X-API-KEY" in str(mock_httpx.delete.call_args[1])
+
+
+@mock.patch("src.routers.redirect.httpx")
+def test_delete_redirect_400_api_inexistente(mock_httpx):
+    app.dependency_overrides[get_current_active_user] = succesful_get_current_active_user
+    response = client.delete("/redirect/api_test/test")
+    assert response.status_code == 400
+    assert not mock_httpx.called
